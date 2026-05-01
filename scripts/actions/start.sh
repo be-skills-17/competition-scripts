bf08f4c5cf2ab361c40ab2fa58197940a5cec2b5
Emilien Marquegnies <marquegniesemilien@gmail.com>
2026-05-01 23:31:39 +0200

    Corrected : start and stop so that it really stop and start all containers related to the infrastructure

--- scripts/actions/start.sh
+++ scripts/actions/start.sh
@@ -6,4 +6,6 @@ docker compose -f $DOCKER_DIR/gitea-runner.yaml start
 docker compose -f $DOCKER_DIR/mysql.yaml start
 docker compose -f $DOCKER_DIR/verdaccio.yaml start
 docker compose -f $DOCKER_DIR/wud.yaml start
+docker compose -f $DYNAMIC_DOCKER_DIR/competitors.yaml start
+
 
--- scripts/actions/stop.sh
+++ scripts/actions/stop.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
-if [ -f .env ]; then
-  set -a
-  # shellcheck disable=SC1090
-  source .env
-  set +a
-fi
-
-docker compose -f $DOCKER_DIR/gitea-runner.yaml stop 
-GITEA_HOSTNAME=$DOMAIN docker compose -f $DOCKER_DIR/gitea.yaml stop 
-docker compose -f $DOCKER_DIR/traefik.yaml stop 
-docker compose -f $DOCKER_DIR/wud.yaml stop
+docker compose -f $DOCKER_DIR/traefik.yaml stop
+docker compose -f $DOCKER_DIR/gitea.yaml stop
+docker compose -f $DOCKER_DIR/gitea-runner.yaml stop
+docker compose -f $DOCKER_DIR/mysql.yaml stop
 docker compose -f $DOCKER_DIR/verdaccio.yaml stop
+docker compose -f $DOCKER_DIR/wud.yaml stop
+docker compose -f $DYNAMIC_DOCKER_DIR/competitors.yaml stop
